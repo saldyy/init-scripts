524fe54601057f419d027fe9beccbd0c0b28fd93
Phillip Nguyen <png9981@gmail.com>
2026-01-19 22:15:56 +0700

    feat: add script install docker

---
+++ install_docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo dnf remove -y docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+sudo dnf -y install dnf-plugins-core
+
+sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable --now docker
+
+sudo usermod -aG docker $USER
+
